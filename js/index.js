a1a9cf7f90cd21043e7547f042378b0613ea7b86
nehad-elsayed <nehad.boody74@gmail.com>
2024-11-14 08:25:43 +0200

    by nehad

---
+++ js/index.js
@@ -0,0 +1,36 @@
+
+
+var authors = [
+    {
+      name: "― Oscar Wilde",
+      quoteText: "“Be yourself; everyone else is already taken.”",
+    },
+    {
+      name: "― Sarah Kay",
+      quoteText:
+        "“I have seen the best of you, and the worst of you, and I choose both.”",
+    },
+  
+    {
+      name: "― Joyce Carol Oates",
+      quoteText: "“Keep a light, hopeful heart. But ­expect the worst.”",
+    },
+    
+    {
+     name:" Mahatma Gandhi",
+     quoteText:"“Be the change that you wish to see in the world.”"
+    },
+    {
+         name:" ― Mark Twain",
+     quoteText:"“If you tell the truth, you don't have to remember anything.”"
+    }
+  ];
+  
+  function displayQuotes() {
+    var randomNum =Math.floor(Math.random() * (authors.length)) ;
+    document.getElementById("text").innerHTML= `${authors[randomNum].quoteText}`;
+    document.getElementById("name").innerHTML= ` ${authors[randomNum].name} `;
+  
+}
+
+
